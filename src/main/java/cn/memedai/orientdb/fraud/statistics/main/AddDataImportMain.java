6b8abe052851b852382d63e34e217a4228e79c04
yu.hang <yu.hang@nan-mi.cn>
2017-05-10 18:09:46 +0800

    将业务处理逻辑抽离至工具类

--- src/main/java/cn/memedai/orientdb/fraud/statistics/main/AddDataImportMain.java
+++ src/main/java/cn/memedai/orientdb/fraud/statistics/main/AddDataImportMain.java
@@ -21,12 +21,47 @@ import java.util.List;
 public class AddDataImportMain {
     private static final Logger LOGGER = LoggerFactory.getLogger(AddDataImportMain.class);
 
-    public void getApplyphonetag (List<String> applyNos){
+    public static void getApplyphonetag (List<String> applyNoList){
+
         Connection conn = (OrientJdbcConnection) OrientDbUtils.getConnection(ConfigUtils.getProperty("orientDbSourceUrl"),
                 ConfigUtils.getProperty("orientDbUserName"), ConfigUtils.getProperty("orientDbUserPassword"));
         ;
         Connection mysqlConn = DbUtils.getConnection(ConfigUtils.getProperty("mysqlDbSourceUrl"),
                 ConfigUtils.getProperty("mysqlDbUserName"), ConfigUtils.getProperty("mysqlDbUserPassword"));
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        List<String> applyNos = new ArrayList<String>();
+        try {
+            for (String applyNo : applyNoList) {
+                pstmt = conn.prepareStatement("select orderinfo.orderNo as orderNo from (MATCH{class:Apply,where: (applyNo = ?)}-ApplyHasOrder->{as:orderinfo,class:Order} return orderinfo)");
+                pstmt.setString(1, applyNo);
+                rs = pstmt.executeQuery();
+                if (rs.next()){
+                    //如果此申请单号有订单号则暂不跑，等订单传递来再进行跑批
+                }else{
+                    applyNos.add(applyNo);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != pstmt){
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != rs){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
         if (null != applyNos && applyNos.size() > 0) {
             SqlUtils.getApplyphonetag(applyNos, conn, mysqlConn);
@@ -50,7 +85,7 @@ public class AddDataImportMain {
         }
     }
 
-    public void getOrderphonetag (List<String> orderNoList){
+    public static void getOrderphonetag (List<String> orderNoList){
         Connection conn = (OrientJdbcConnection) OrientDbUtils.getConnection(ConfigUtils.getProperty("orientDbSourceUrl"),
                 ConfigUtils.getProperty("orientDbUserName"), ConfigUtils.getProperty("orientDbUserPassword"));
         ;
@@ -68,12 +103,12 @@ public class AddDataImportMain {
                     pstmt = conn.prepareStatement("select applyinfo.applyNo as applyNo from (MATCH{class:Order,where: (orderNo = ?)}<-ApplyHasOrder-{as:applyinfo,class:Apply} return applyinfo)");
                     pstmt.setString(1, orderNo);
                     rs = pstmt.executeQuery();
-                    while (rs.next()){
+                    if (rs.next()){
                         if (null != rs.getString("applyNo")){
                             applyNosHasOrders.add(rs.getString("applyNo"));
-                        }else{
-                            orderNos.add(orderNo);
                         }
+                    }else{
+                        orderNos.add(orderNo);
                     }
                 }
         } catch (SQLException e) {
@@ -125,5 +160,17 @@ public class AddDataImportMain {
             }
             LOGGER.info("AddDataImportMain getApplyphonetag   mysqlConn.close() end");
         }
+    }
+
+    public static void main(String[] args) {
+        List<String> applyNos = new ArrayList<String>();
+        applyNos.add("2015112710026510");
+        applyNos.add("1489306018145002");
+        getApplyphonetag(applyNos);
+
+        List<String> orderNos = new ArrayList<String>();
+        orderNos.add("2015112710026510");
+        orderNos.add("2015080210072710");
+        getOrderphonetag(orderNos);
     }
 }
