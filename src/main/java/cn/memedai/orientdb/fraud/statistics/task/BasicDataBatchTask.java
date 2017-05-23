3becbdb6018e512c27fa2a676160e59463274e8b
ti zhou <ti.zhou@nan-mi.cn>
2017-05-23 14:41:08 +0800

    去除多余;

--- src/main/java/cn/memedai/orientdb/fraud/statistics/task/BasicDataBatchTask.java
+++ src/main/java/cn/memedai/orientdb/fraud/statistics/task/BasicDataBatchTask.java
@@ -1,15 +1,12 @@
 package cn.memedai.orientdb.fraud.statistics.task;
 
-import cn.memedai.orientdb.fraud.statistics.bean.ApplyAndOrderDate;
 import cn.memedai.orientdb.fraud.statistics.bean.MemberAndPhoneBean;
-import cn.memedai.orientdb.fraud.statistics.main.AddDataImportMain;
 import cn.memedai.orientdb.fraud.statistics.utils.SqlUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by hangyu on 2017/5/12.
